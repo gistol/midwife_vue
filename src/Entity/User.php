28bbee078f86183f1e358a81a4a7788348a0c9be
bluznierca1 <grzegorz.lasak@grzegorzlasak.com>
2018-05-25 22:12:18 +0100

    Small push with small edits

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,10 +5,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-/**
- * @ORM\Table(name="user")
- * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
- */
+
 class User implements UserInterface
 {
     /**
